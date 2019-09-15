c4748befadf873e802a0f5dbadd998c961644dfa
Shailendra555 <shailendrapurohit2010@gmail.com>
2019-09-15 13:17:04 +0530

    change appname again

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/fyle-bank-detail'));
+app.use(express.static('./dist/bank-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/fyle-bank-detail/index.html'));
+res.sendFile(path.join(__dirname,'/dist/bank-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
